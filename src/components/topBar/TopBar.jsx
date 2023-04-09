f0b2bcb26abb15daeab2e2e5d51e21e59e1c0ee4
dev-moshiur <dev.moshiurr@gmail.com>
2023-04-10 02:48:56 +0600

    commited

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -95,8 +95,10 @@ export default function TopBar() {
             </div>
           </div>
           <div className="about">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquid quidem nostrum quisquam obcaecati atque ipsum quo. Ipsam, voluptatibus! Consequatur magni,  culpa quisquam
-            
+          Hello! My name is Md Moshiur Rahman Masud and I am a <strong> MERN stack developer</strong>. I began my journey in 2020 during the challenging time of the COVID-19 pandemic. Since then, I have honed my skills in the <strong> MERN (MongoDB, Express.js, React.js, and Node.js) stack</strong>, which is a powerful and popular technology stack for building web applications. I have a strong foundation in front-end development with<strong>React.js</strong>  and back-end development with <strong>Node.js and Express.js</strong> , as well as experience working with <strong>MongoDB(Mongoose)</strong>  for database management. I am passionate about creating efficient and user-friendly web applications that provide a seamless experience for users. I am excited to continue my journey as a MERN stack developer and contribute to innovative projects in the tech industry.
+          <br/>
+          <br/>
+          Feel free to contact me if you need a stack website on MERN stack technology
           </div>        
         </div>
         <div className="clear" onClick={()=>setShowDeveloper(false)} >
